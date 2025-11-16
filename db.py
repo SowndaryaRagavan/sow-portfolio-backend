fd2cf9ac5d88be94d1880403207f2c508be93840
Sowndarya <sowndarya1024@gmail.com>
2025-11-16 21:07:41 +0800

    Fixing ESlint issue2 for Backend

--- db.py
+++ db.py
@@ -4,9 +4,11 @@ from sqlalchemy.orm import sessionmaker
 
 # Replace this URL with your Neon database URL
 SQLALCHEMY_DATABASE_URL = (
-    "postgresql://neondb_owner:npg_ks2jo6taYDZF@ep-floral-poetry-ahdnyg9y-pooler.c-3.us-east-1.aws.neon.tech/"
+    "postgresql://neondb_owner:npg_ks2jo6taYDZF@"
+    "ep-floral-poetry-ahdnyg9y-pooler.c-3.us-east-1.aws.neon.tech/"
     "neondb?sslmode=require&channel_binding=require"
 )
+
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True)
 
